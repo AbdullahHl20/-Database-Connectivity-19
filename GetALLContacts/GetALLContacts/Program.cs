6f02ad6a9bd4ab79527af81ee0a2632846b2c022
Abdullah Alhelal <abdullah.h@alameensoft.com>
2025-02-16 06:03:51 -0800

    Parameterized Query With "Like"

--- GetALLContacts/GetALLContacts/Program.cs
+++ GetALLContacts/GetALLContacts/Program.cs
@@ -10,9 +10,10 @@ namespace GetALLContacts
     internal class Program
     {
         public static string connectionString = @"server=DTERMINAL105\MSSQLSERVER2022;User Id=SSDEV\abdullah;database=ContactsDB;Integrated Security=True;";
-       
+
 
         static void PrintAllContacts(string FirstName)
+
         {
 
             SqlConnection connection = new SqlConnection(connectionString);
@@ -56,13 +57,57 @@ namespace GetALLContacts
             {
                 Console.WriteLine("Error: " + ex.Message);
             }
-
-
         }
 
+        static void SercheinFirstNameAllContacts(string FirstName)
+        {
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            string query = "SELECT * FROM Contacts where FirstName like '' + @FirstName  + '%'";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@FirstName", FirstName);
+            try
+            {
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int contactID = (int)reader["ContactID"];
+                    string firstName = (string)reader["FirstName"];
+                    string lastName = (string)reader["LastName"];
+                    string email = (string)reader["Email"];
+                    string phone = (string)reader["Phone"];
+                    string address = (string)reader["Address"];
+                    int countryID = (int)reader["CountryID"];
+
+                    Console.WriteLine($"Contact ID: {contactID}");
+                    Console.WriteLine($"Name: {firstName} {lastName}");
+                    Console.WriteLine($"Email: {email}");
+                    Console.WriteLine($"Phone: {phone}");
+                    Console.WriteLine($"Address: {address}");
+                    Console.WriteLine($"Country ID: {countryID}");
+                    Console.WriteLine();
+                }
+
+                reader.Close();
+                connection.Close();
+
+            }
+
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+        }
         static void Main(string[] args)
         {
-            PrintAllContacts("John");
+            SercheinFirstNameAllContacts("j");
             Console.ReadKey();
         }
     }
